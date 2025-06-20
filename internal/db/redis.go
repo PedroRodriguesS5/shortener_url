0e21e754b6d7965b2cd805072c947b2ebae324e2
Pedro Silva <pedroxbrs@gmail.com>
2025-06-20 20:51:21 -0300

    Feat<internal/db>:Create the initial db redis structure

---
+++ internal/db/redis.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var Ctx = context.Background()
+var Rdb *redis.Client
+
+func InitRedis() {
+	db, _ := strconv.Atoi(os.Getenv("DB"))
+	Rdb = redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_URL"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       db,
+	})
+
+	_, err := Rdb.Ping(Ctx).Result()
+	if err != nil {
+		log.Fatal("Error connecting to Redis", err)
+	}
+}
+
+func SaveURL(code, url string, expired int) error {
+	return Rdb.Set(Ctx, code, url, time.Duration(expired)*time.Second).Err()
+}
+
+func GetURL(code string) (string, error) {
+	return Rdb.Get(Ctx, code).Result()
+}
+
+func IncrementClick(code string) error {
+	return Rdb.Incr(Ctx, "clicks:"+code).Err()
+}
+
+func GetClicks(code string) (int64, error) {
+	return Rdb.Get(Ctx, "clicks:"+code).Int64()
+}
---
+++ internal/httpClient/handler.go
@@ -0,0 +1,60 @@
+package httpclient
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/pedrorodrigues5/shorter_url/model"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pedrorodrigues5/shorter_url/internal/db"
+	"github.com/pedrorodrigues5/shorter_url/utils"
+)
+
+func ShortenerURL(c *gin.Context) {
+	var req model.URLMapping
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(400, gin.H{"error": "Custom short code already exists"})
+		return
+	}
+
+	code := req.Custom
+
+	if code == "" {
+		code = utils.GenerateShortCode(6)
+	}
+
+	// Check if custom code already exists
+	if _, err := db.GetURL(code); err == nil {
+		c.JSON(400, gin.H{"error": "Custom short code already exists"})
+		return
+	}
+
+	expiry := req.ExpiresIn
+	if expiry == 0 {
+		expiry = 3600 * 24 * 7
+	}
+
+	if err := db.SaveURL(code, req.URL, expiry); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save the url"})
+		return
+	}
+
+	c.JSON(http.StatusOK, model.ShortenResponse{
+		ShortURL: os.Getenv("BASE_URL") + "/" + code,
+	})
+
+}
+
+func ResolveURL(c *gin.Context) {
+	code := c.Param("code")
+
+	url, err := db.GetURL(code)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "URL not found"})
+		return
+	}
+
+	db.IncrementClick(code)
+	c.Redirect(http.StatusFound, url)
+}
