76e6b6cdf5c9100cec4cc31fcc663cc9b00ef776
Pedro Silva <pedroxbrs@gmail.com>
2025-06-20 20:52:34 -0300

    Feat<utils>:Create the shorten url logic

--- utils/shortener.go
+++ utils/shortener.go
@@ -1,1 +1,20 @@
 package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func Init() {
+	new(rand.Rand).Seed(time.Now().UnixNano())
+}
+
+func GenerateShortCode(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
